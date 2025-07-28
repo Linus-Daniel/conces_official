4536e8befd108e0a406e17925c495f9343f32d83
Linus-Daniel <vandulinus@gmail.com>
2025-07-28 08:10:25 +0100

    new

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -7,7 +7,7 @@ const hostname = isBrowser ? window.location.hostname : "localhost";
 // Dynamically build baseURL based on the device
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https:conces-official.vercel.app/api"
+    ? "https://conces-official.vercel.app/api"
     : `http://${hostname}:3000/api`;
 
 const token = isBrowser ? localStorage.getItem("token") : null;
